4a347f7ac8ededd2ca24edf527ede42c7bc8ba05
amelialutz9 <amelialutz9@gmail.com>
2017-12-01 17:44:07 -0600

    Delete Test.java

--- Test.java
+++
@@ -1,9 +0,0 @@
-public class Test {
-  public static void main(String[] args){
-    System.out.println("Hello, world.");
-    System.out.println("hey");
-    int x = 2;
-    int y = 0;
-    int z = x - y;
-  }
-}
